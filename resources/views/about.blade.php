053780ff1c28a9635d3c55931c1a9e01eb1cd8de
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2025-05-29 22:49:21 +0700

    Latihan2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
-     <a href="/home">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-</body>
-</html>
+<x-layout>  
+     <x-slot:title>{{ $title }}</x-slot:title>  
+  <h3 class="text-xl">About me</h3>
+    <p>
+        Nama : {{ $nama }}
+    </p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Homepage']);
 });
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Nosferatu']);
+    return view('about', ['nama' => 'Nosferatu'], ['title' => 'About me']);
 });
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact me']);
 });
