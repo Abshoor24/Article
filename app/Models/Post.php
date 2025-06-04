9a19d3a8cd734384332e035e25a1c7db62ec27c9
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2025-06-04 21:59:11 +0700

    Latihan6

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,11 @@
 namespace App\Models;// untuk mengasih tau bahwa file ini berada di dalam namespace App\Models
 // namespace ini digunakan untuk mengorganisir kode dan menghindari konflik nama kela   s
 use Illuminate\Database\Eloquent\Model; // Model adalah kelas dasar untuk semua model Eloquent di Laravel
+use Illuminate\Database\Eloquent\Factories\HasFactory; // trait HasFactory digunakan untuk mengaktifkan fitur factory pada model ini
 
 class Post extends Model
 {
+     use HasFactory; // trait HasFactory digunakan untuk mengaktifkan fitur factory pada model ini
+
      protected $fillable = ['title', 'author', 'slug', 'body'];// untuk mengatur atribut yang dapat diisi secara massal
 }
